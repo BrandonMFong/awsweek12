e77c18d57d918226aab1b004bf4003e2e748eb60
BrandonMFong <bfong@owc.com>
2021-11-21 22:46:56 +0000

    Removing worker node

--- ec2.tf
+++ ec2.tf
@@ -15,26 +15,6 @@
   }
 }*/
 
-# worker vm
-resource "aws_instance" "week12-worker-vm" {
-  ami                  = "ami-0168b9285893a7395"
-  instance_type        = "t2.micro"
-  subnet_id            = aws_subnet.week12-pri-a.id
-  iam_instance_profile = aws_iam_instance_profile.week12-profile-ref.name
-
-  vpc_security_group_ids = [
-    aws_security_group.week12-ssh-pri-sg.id
-  ]
-
-  key_name = "ECE592"
-
-  user_data = file("cloudinit.txt")
-
-  tags = {
-    Name = "week12-worker-vm"
-  }
-}
-
 # IAM profile ref
 resource "aws_iam_instance_profile" "week12-profile-ref" {
   name = "week12-profile-ref"
