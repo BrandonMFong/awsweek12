7ceb2ce7783a1ff8bd8445589c3d771ac0e980a4
BrandonMFong <bfong@owc.com>
2021-11-22 05:57:18 +0000

    Adding sns description

--- sns.tf
+++ sns.tf
@@ -4,3 +4,9 @@ resource "aws_sns_topic" "week12-sns" {
   kms_master_key_id = "ECE592"
 }
 
+resource "aws_sns_topic_subscription" "week12-sns-mail" {
+  topic_arn = aws_sns_topic.week12-sns.id
+  protocol  = "email"
+  endpoint  = "fong.m.brandon97@gmail.com"
+}
+
